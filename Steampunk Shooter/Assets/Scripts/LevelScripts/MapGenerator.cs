da1fcacd4a7f6e373600d45d3cdd7010ff52edad
Billy Bao <billy_bao@126.com>
2021-12-01 15:14:56 -0800

    add LongLevel to build and test; fix tilemap layering

--- Steampunk Shooter/Assets/Scripts/LevelScripts/MapGenerator.cs
+++ Steampunk Shooter/Assets/Scripts/LevelScripts/MapGenerator.cs
@@ -14,17 +14,25 @@ public class MapGenerator : MonoBehaviour
     public Map GenerateMap(int size)
     {
         Map map = new Map(10, 10);
-        LevelNode n1 = new LevelNode("CustomLevelB", new Map.Coords(4, 4));
+        LevelNode n1 = new LevelNode("LongLevel", new Map.Coords(4, 4));
         LevelFlags fl = new LevelFlags();
-        fl.disableTop = true;
-        fl.disableRight = true;
         map.AddLevel(n1, fl);
 
-        LevelNode n2 = new LevelNode("CustomLevelB", new Map.Coords(3, 4));
+        LevelNode n2 = new LevelNode("CustomLevelB", new Map.Coords(4, 5));
         fl = new LevelFlags();
-        fl.disableTop = true;
         fl.disableLeft = true;
         map.AddLevel(n2, fl);
+
+        LevelNode n3 = new LevelNode("LongLevel", new Map.Coords(4, 3));
+        fl = new LevelFlags();
+        fl.disableTop = true;
+        map.AddLevel(n3, fl);
+
+        LevelNode n4 = new LevelNode("CustomLevelB", new Map.Coords(5, 5));
+        fl = new LevelFlags();
+        fl.disableTop = true;
+        fl.disableRight = true;
+        map.AddLevel(n4, fl);
 
         return map;
     }
