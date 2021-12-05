4d1d9034caf3ab780d0ac05b72a76d415c544fc6
Billy Bao <billy_bao@126.com>
2021-12-04 17:33:14 -0800

    fog trigger to hide secret area

---
+++ Steampunk Shooter/Assets/Scripts/LevelScripts/FogTriggerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FogTriggerScript : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("Fog").SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
