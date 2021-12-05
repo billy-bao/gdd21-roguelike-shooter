7fc1c4587affa64f9772551d1216ee62c0006602
Sam Ghahremani <sam.ghahremani8@gmail.com>
2021-12-05 00:38:35 -0800

    Added SoundManager Script

---
+++ Steampunk Shooter/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource BGM;
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeBGM(AudioClip music)
+    {
+        Debug.Log("CHANGING MUSIC");
+        this.BGM.Stop();
+        this.BGM.clip = music;
+        this.BGM.Play();
+    }
+}
